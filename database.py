fc7419eaca140cd622f7d09782c46e3cbb2d53db
ducmanht6-hub <ducmanht6@gmail.com>
2026-07-08 21:25:41 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:123456%40@127.0.0.1:3306/shop_db"
+DATABASE_URL = "mysql+pymysql://root:123456@127.0.0.1:3306/shop_db"
 
 engine = create_engine(DATABASE_URL)
 
